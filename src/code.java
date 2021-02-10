16e069cfc23f5e6895a813f1191cc1e15d1a988e
markusfaglum <markusfaglum@gmail.com>
2021-02-10 13:36:26 +0000

    Added code to print out a short text

--- src/code.java
+++ src/code.java
@@ -1,2 +1,9 @@
 public class code {
+
+    static int color = 20;
+    static String name = "jennifer";
+
+    public static void main(String[] args) {
+        System.out.println("my cat is " + color + name);
+    }
 }
